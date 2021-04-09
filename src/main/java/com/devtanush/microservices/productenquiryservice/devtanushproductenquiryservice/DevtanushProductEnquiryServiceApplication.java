b12f4516ebe86536faf694c423aa71f1a63b31c3
ntanui <ntanui@safaricom.co.ke>
2021-04-09 19:20:05 +0300

    init setup

---
+++ src/main/java/com/devtanush/microservices/productenquiryservice/devtanushproductenquiryservice/DevtanushProductEnquiryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.productenquiryservice.devtanushproductenquiryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtanushProductEnquiryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtanushProductEnquiryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtanush/microservices/productenquiryservice/devtanushproductenquiryservice/DevtanushProductEnquiryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.productenquiryservice.devtanushproductenquiryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtanushProductEnquiryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
