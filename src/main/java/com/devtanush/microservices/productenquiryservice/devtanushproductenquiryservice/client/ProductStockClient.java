9fc93a4f0b69888b2e1a21b9f2d6f2413f69fdc4
ntanui <ntanui@safaricom.co.ke>
2021-04-10 12:54:45 +0300

    addribbon configs

--- src/main/java/com/devtanush/microservices/productenquiryservice/devtanushproductenquiryservice/client/ProductStockClient.java
+++ src/main/java/com/devtanush/microservices/productenquiryservice/devtanushproductenquiryservice/client/ProductStockClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "devtanush-product-stock-service",url = "localhost:8800")
+//@FeignClient(name = "devtanush-product-stock-service",url = "localhost:8800")
+@FeignClient(name = "devtanush-product-stock-service")
 public interface ProductStockClient {
 
     @GetMapping("/check-product-stock/productName/{productName}/productAvailability/{productAvailability}")
